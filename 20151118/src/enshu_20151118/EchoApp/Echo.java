30f37566ce7bbc496da328761e138096e2a525ce
Shingo Kitagawa <shingogo@hotmail.co.jp>
2015-12-02 10:23:52 +0900

    assignment 1 to 4

---
+++ 20151118/src/enshu_20151118/EchoApp/Echo.java
@@ -0,0 +1,13 @@
+package EchoApp;
+
+
+/**
+* EchoApp/Echo.java .
+* IDL-to-Javaコンパイラ(ポータブル)、バージョン"3.2"によって生成されました
+* echo.idlから
+* 2015年12月2日 9時39分07秒 JST
+*/
+
+public interface Echo extends EchoOperations, org.omg.CORBA.Object, org.omg.CORBA.portable.IDLEntity 
+{
+} // interface Echo
---
+++ 20151118/src/enshu_20151118/EchoApp/EchoOperations.java
@@ -0,0 +1,14 @@
+package EchoApp;
+
+
+/**
+* EchoApp/EchoOperations.java .
+* IDL-to-Javaコンパイラ(ポータブル)、バージョン"3.2"によって生成されました
+* echo.idlから
+* 2015年12月2日 9時39分07秒 JST
+*/
+
+public interface EchoOperations 
+{
+  void echoString (String input);
+} // interface EchoOperations
